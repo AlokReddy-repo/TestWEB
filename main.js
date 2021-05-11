be409c2d1c7f22ba13e984887942c53c6dced095
AlokReddy-repo <83569955+AlokReddy-repo@users.noreply.github.com>
2021-05-11 16:04:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+
+var a = 1; //eslint-disable-line no-unused-vars
+var b = "i am teaching you this course";
+var c = "hello guys";
+var mygoal = 'learn ASAP';
+document.write (mygoal + ' ' + b +' ' + c);
+document.write ('<br>');
+document.write (c + c + b);
+
+
+
+
+function hellobuddy(){ //eslint-disable-line no-unused-vars
+		alert('hello bro'); // still raises defined but never used
+}
