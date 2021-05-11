c1279bb28ebc0f71e77b9068578df41d16a46cf5
AlokReddy-repo <83569955+AlokReddy-repo@users.noreply.github.com>
2021-05-11 16:12:07 +0530

    neww

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 
 var a = 1; //eslint-disable-line no-unused-vars
-var b = "i am teaching you this course";
-var c = "hello guys";
-var mygoal = 'learn ASAP';
-document.write (mygoal + ' ' + b +' ' + c);
-document.write ('<br>');
-document.write (c + c + b);
+var b = "I am teaching you this course";
+var c = "Hello Guys";
+var mygoal = 'Learn ASAP';
+document.write (c + ' ' + b + ' ' + mygoal);
 
 
 
