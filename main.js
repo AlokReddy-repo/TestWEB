0ce816ba81dd7e82b40b8a668c36dde301a09d80
AlokReddy-repo <83569955+AlokReddy-repo@users.noreply.github.com>
2021-05-13 12:55:07 +0530

    final changes

--- main.js
+++ main.js
@@ -2,88 +2,136 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-constant-condition */
 /* eslint-disable no-redeclare */
-var a = 1; 
+/* eslint-disable no-empty */
+/* eslint-disable no-undef */
+
+var a = 1;
 var b = "I am teaching you this course";
 var c = "Hello Guys";
 var mygoal = 'Learn ASAP';
 document.write(c + ' ' + b + ' ' + mygoal);
 
-var blank = ""; 
-var firstname = "Alok"; 
-var lastname = "Reddy"; 
-var Array = ['first item', 'second',3,4,5, 'six', 7];
+var blank = "";
+var firstname = "Alok";
+var lastname = "Reddy";
+
+var Array = ['first item', 'second', 3, 4, 5, 'six', 7];
+Array.push('ten');
+
+
 var output = document.getElementById('output');
-var fullname = firstname + ' ' + lastname; 
+var fullname = firstname + ' ' + lastname;
+
+
+var person = {
+  age: 25,
+  firstname: 'Alok',
+  lastname: 'Reddy',
+  fullname: function () {
+    return this.firstname + ' ' + this.lastname
+  }
+};
+
 output.innerHTML = Array[1];
 output.innerHTML = firstname;
-/*output.innerHTML= fullname;*/
-Array.push ('ten');
-function selectArray(a){
-	return Array[a];
+output.innerHTML= fullname;
+
+
+var beta = 'SDFNJKIUYTRDLMYSAS';
+function splitstring(a){
+	var e = a.split('');
+	var f = e.sort().join('');
+	return f;
+}
+newOutput(splitstring(beta))
+
+var alpha = 'Array list';
+
+for (var x = 0; x < Array.length; x++) {
+  alpha = alpha + '<br>' + Array[x];
 }
 
-function myname(a, b) { 
+newOutput(alpha)
+function alpha(myOutput) {
+  output.innerHTML = myOutput;
+}
+
+function selectArray(a) {
+  return Array[a];
+}
+
+function myname(a, b) {
   var fullname = a + ' ' + b;
   return fullname;
 }
 output.innerHTML = myname(firstname, lastname);
 
-function hellobuddy() { 
-  alert('hello bro'); 
+function hellobuddy() {
+  alert('hello bro');
 }
 
 
-function newOutput(myOutput){ 
-	output.innerHTML = myOutput;
+function newOutput(myOutput) {
+  output.innerHTML = myOutput;
 }
+
 /* newOutput('it is working'); */
+
 var one = '8';
- if(one == 2){
-	 newOutput('Computer'); //true
- }else{
-	 newOutput('User'); //false
- }
+if (one == 2) {
+  newOutput('Computer'); //true
+} else {
+  newOutput('User'); //false
+}
 
 
- if('2' != 2){
-	 newOutput('Computer'); //true
- }else{
-	 newOutput('User'); //false
- }
+if ('2' != 2) {
+  newOutput('Computer'); //true
+} else {
+  newOutput('User'); //false
+}
 
 
- if(one > 2){
-	 newOutput('Computer'); //true
- }else{
-	 newOutput('User'); //false
- }
+if (one > 2) {
+  newOutput('Computer'); //true
+} else {
+  newOutput('User'); //false
+}
 
- if(one <= 2 && one <= 8){
-	 newOutput('Computer'); //true
- }else{
-	 newOutput('User'); //false
- }
+if (one <= 2 && one <= 8) {
+  newOutput('Computer'); //true
+} else {
+  newOutput('User'); //false
+}
 
-function myChoice(a){
-	var b = '';
-	if (a>0 && a<=0.33){
-		b='Alok Reddy';
-	}else if(a>0.33 && a<=0.66){
-		b='Bot';
-	}else if(a>0.66 && a<=1){
-			b='Trojan';
-		}
-	return b;
+function myChoice(a) {
+  var b = '';
+  if (a > 0 && a <= 0.33) {
+    b = 'Alok Reddy';
+  } else if (a > 0.33 && a <= 0.66) {
+    b = 'Bot';
+  } else if (a > 0.66 && a <= 1) {
+    b = 'Trojan';
+  }
+  return b;
 }
 newOutput(myChoice(0.99));
-/* newOutput(myChoice(Math.random())); */
+
+newOutput(myChoice(Math.random()));
 
 var myRandom = Math.random();
-newOutput(myRandom + '<br>'+ myChoice(myRandom));
+newOutput(myRandom + '<br>' + myChoice(myRandom));
 
 newOutput(selectArray(7));
+newOutput(Array.length);
+
+newOutput(person.fullname());
 
 
+
+
+/* eslint-enable no-undef */
+/* eslint-enable no-empty */
 /* eslint-enable no-redeclare */
 /* eslint-enable no-constant-condition */
 /* eslint-enable no-unused-vars */
